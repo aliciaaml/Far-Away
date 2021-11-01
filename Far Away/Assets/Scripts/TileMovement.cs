068843a8eb2a512ffd52f42128079b9d2b493c26
Swappppy <francft1@gmail.com>
2021-11-01 21:56:07 +0100

    gameobject selection on click using raycast (working)

--- Far Away/Assets/Scripts/TileMovement.cs
+++ Far Away/Assets/Scripts/TileMovement.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class TileMovement : MonoBehaviour
 {
     [SerializeField] private GameObject EmptyTile;
+    private Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit)
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
     }
 }
