3bc5de1591d5b1c81a14a706eaa155b724ad9620
aliciaaml <90557106+aliciaaml@users.noreply.github.com>
2021-11-04 13:23:25 +0100

    Add files via upload

---
+++ Far Away/Assets/Minijuego1/Scripts/Movimiento_piñones3.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movimiento_piñones3 : MonoBehaviour
+{
+
+    public Movimiento_piñones mov;
+
+    public float velocidad;
+
+    public AudioClip sound_engranajes;
+    public AudioClip sound_correcto;
+
+    float dirY;
+
+    public Rigidbody2D piñon3;
+
+    AudioSource audioSource;
+
+
+    bool colis;
+ 
+    bool aux;
+
+    public GameObject position;
+
+
+   void Start(){
+
+        piñon3= GetComponent<Rigidbody2D>();
+
+        audioSource= GetComponent<AudioSource>();
+        
+        aux=false;
+        colis=false;
+        
+       
+   }
+   void OnCollisionEnter2D (Collision2D colision){
+       
+
+       if (colision.gameObject.name==position.name){
+            audioSource.Stop();
+            audioSource.PlayOneShot(sound_correcto);
+            colis=true;
+       }
+   }
+    void Update(){
+
+        dirY=Input.GetAxis("Vertical");
+
+        if(mov.num_colis==2){
+
+            position.name="Pos_correcta3";
+            Arrows(piñon3);
+        }
+            
+
+
+    }
+
+
+    void Arrows(Rigidbody2D rig){
+        
+        if(Input.GetKey(KeyCode.UpArrow)|| Input.GetKey(KeyCode.DownArrow)) {
+
+            if(colis==false){
+                Vector2 position=rig.position;
+                position.y=position.y+velocidad*dirY;
+
+                rig.MovePosition(position);
+
+                if (aux==false){
+
+                    audioSource.PlayOneShot(sound_engranajes);
+                    aux=true;
+                }
+                   
+            }
+            
+            
+        }
+        else {
+            audioSource.Stop();
+            aux=false;
+        }
+
+        if(colis){
+
+            if(Input.GetKey(KeyCode.RightArrow)){
+
+                    mov.num_colis=3;
+
+                    
+            }
+                
+                    
+        }
+     
+    }
+}
