486d379ad91088ee582764b37da432ba448c72a5
VnssJT <64807616+VnssJT@users.noreply.github.com>
2021-11-05 12:28:40 +0100

    mapa

--- Far Away/Assets/Scripts/CargarEscenasBoton.cs
+++ Far Away/Assets/Scripts/CargarEscenasBoton.cs
@@ -2,26 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CargarEscenasBoton : MonoBehaviour
 {
+    public Image theButton; // Aquí se pone el mismo botón que tenga este script
+    public static string escenaActual;
+    public static string escenaAnterior;
 
     public void LoadScene(string sceneName)
     {
+        escenaAnterior = SceneManager.GetActiveScene().name;
+        escenaActual = sceneName;
         SceneManager.LoadScene(sceneName);
     }
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void volverAAnterior(){
+        SceneManager.LoadScene(escenaAnterior);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
