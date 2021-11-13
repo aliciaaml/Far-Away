0ccd2b317fb42f5e1e0c47ed6b35382ec5fdcae2
Swappppy <francft1@gmail.com>
2021-11-13 23:30:07 +0100

    inventory shows icon

--- Far Away/Assets/Scripts/Inventario y objetos/inventoryUI.cs
+++ Far Away/Assets/Scripts/Inventario y objetos/inventoryUI.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class inventoryUI : MonoBehaviour
 { 
     Inventory inventory;
+    public Transform Parent;
+
+    Slot[] slots;
+
     // Start is called before the first frame update
     void Start()
     {
         inventory = Inventory.inst;
         inventory.OnItemChangeCallback += UpdateUI;
+
+        slots = Parent.GetComponentsInChildren<Slot>();
     }
 
     // Update is called once per frame
@@ -18,6 +24,16 @@ public class inventoryUI : MonoBehaviour
 
     void UpdateUI()
     {
-
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].CleanSlot();
+            }
+        }
     }
 }
