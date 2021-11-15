5e9c381cb6825a2fe5a131c1f8270bcaf213a2cf
Elisa2001beth <al394783@uji.es>
2021-11-15 21:03:42 +0100

    parallax

---
+++ Far Away/Assets/Scripts/ParallaxEfect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEfect : MonoBehaviour
+{
+    [SerializeField] private float parallaxMultiplier;
+
+    private Transform cameraTransform;
+    private Vector3 previousCameraPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        previousCameraPosition = cameraTransform.position;
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float deltaX = (cameraTransform.position.x - previousCameraPosition.x) * parallaxMultiplier;
+        transform.Translate(new Vector3(deltaX, 0, 0));
+        previousCameraPosition = cameraTransform.position;
+    }
+}
