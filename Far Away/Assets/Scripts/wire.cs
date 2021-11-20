5610914bb95467a8c43caacb606dd293288c84de
Swappppy <francft1@gmail.com>
2021-11-20 19:08:35 +0100

    code tweaks

--- Far Away/Assets/Scripts/wire.cs
+++ Far Away/Assets/Scripts/wire.cs
@@ -6,11 +6,14 @@ public class wire : MonoBehaviour
 {
     public SpriteRenderer WireEnd;
 
+    Vector3 StartParentPos;
     Vector3 StartPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartPos = gameObject.transform.parent.position;
+        StartParentPos = gameObject.transform.parent.position;
+        StartPos = gameObject.transform.position;
     }
 
     // Update is called once per frame
@@ -21,19 +24,30 @@ public class wire : MonoBehaviour
         Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         MousePos.z = 0;     // Ya que las coordenadas de z son -10 por defecto y el valor del input del raton es un vector3
 
+        UpdateWire(MousePos);
+        
+    }
+
+    private void OnMouseUp()
+    {
+        
+    }
+
+    void UpdateWire(Vector3 MousePos)
+    {
         // Actuaizar posición del objeto
 
         gameObject.transform.position = MousePos;
 
         // Actualizar dirección
 
-        Vector3 Direction = MousePos - StartPos;
+        Vector3 Direction = MousePos - StartParentPos;
         gameObject.transform.right = Direction * transform.lossyScale.x;        // Como usamos un cambio de escala para la parte derecha, transfrom.lossyScale hace que las coordenadas en este caso de rotación sean correctas
 
 
         // Actualizar escala
 
-        float dist = Vector2.Distance(StartPos, MousePos);
+        float dist = Vector2.Distance(StartParentPos, MousePos);
         WireEnd.size = new Vector2(dist, WireEnd.size.y);
     }
 }
