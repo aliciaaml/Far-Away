1ccc9884ccbc8bd883ec3b5330cfe824597de692
Swappppy <francft1@gmail.com>
2021-11-20 18:26:09 +0100

    prefab and script tweaks

--- Far Away/Assets/Scripts/wire.cs
+++ Far Away/Assets/Scripts/wire.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class wire : MonoBehaviour
+public class Wire : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -11,8 +11,15 @@ public class wire : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void OnMouseDrag()
     {
-        
+        // Posición del ratón
+
+        Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        MousePos.z = 0;     // yq que las coordenadas de z son -10 por defecto y el valor del input del raton es un vector3
+
+        // Actuaizar posición del objeto
+
+        gameObject.transform.position = MousePos;
     }
 }
