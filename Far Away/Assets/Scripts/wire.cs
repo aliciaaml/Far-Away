25eefed3d7a948a1c2b508d0b5181a08367de5a4
Swappppy <francft1@gmail.com>
2021-11-20 18:43:02 +0100

    wire scale on drag base not working properly

--- Far Away/Assets/Scripts/wire.cs
+++ Far Away/Assets/Scripts/wire.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wire : MonoBehaviour
+public class wire : MonoBehaviour
 {
+    public SpriteRenderer WireEnd;
+
     Vector3 StartPos;
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,7 @@ public class Wire : MonoBehaviour
         // Posición del ratón
 
         Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        MousePos.z = 0;     // yq que las coordenadas de z son -10 por defecto y el valor del input del raton es un vector3
+        MousePos.z = 0;     // Ya que las coordenadas de z son -10 por defecto y el valor del input del raton es un vector3
 
         // Actuaizar posición del objeto
 
@@ -26,6 +28,12 @@ public class Wire : MonoBehaviour
         // Actualizar dirección
 
         Vector3 Direction = MousePos - StartPos;
-        gameObject.transform.right = Direction;
+        gameObject.transform.right = Direction * transform.lossyScale.x;        // Como usamos un cambio de escala para la parte derecha, transfrom.lossyScale hace que las coordenadas en este caso de rotación sean correctas
+
+
+        // Actualizar escala
+
+        float dist = Vector2.Distance(StartPos, MousePos);
+        WireEnd.size = new Vector2(dist, WireEnd.size.y);
     }
 }
