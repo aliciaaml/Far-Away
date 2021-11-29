3f2e99f920df2e8c570bacb6286584b91abe2a51
Swappppy <francft1@gmail.com>
2021-11-29 19:42:33 +0100

    tablones show up when you click on mesa and silla

--- Far Away/Assets/Scripts/Tablones.cs
+++ Far Away/Assets/Scripts/Tablones.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Tablones : MonoBehaviour
@@ -7,6 +5,8 @@ public class Tablones : MonoBehaviour
     public SpriteRenderer Tablon;
     public static int contador;
     [SerializeField] private GameObject[] tablones;
+    [SerializeField] private GameObject mesa;
+    [SerializeField] private GameObject silla;
 
     Vector3 StartPos;
 
@@ -29,47 +29,52 @@ public class Tablones : MonoBehaviour
         // Cmporbar si esta cerca de algun cable para hacer un snap
 
         Collider2D[] colliders = Physics2D.OverlapCircleAll(MousePos, 0.2f);
-        foreach (Collider2D collider in colliders)
+
+        if (!mesa.activeSelf && !silla.activeSelf)
         {
-            if (collider.transform.parent.parent.name.Equals("PosicionesCorrectasVentana") && gameObject.transform.parent.parent.name.Equals("SpritesVentana")) 
+            foreach (Collider2D collider in colliders)
             {
-                UpdateWire(collider.transform.position);    // Actualiza la posición al collider adecuado
-
-                contador += 1;
-
-                Destroy(gameObject.GetComponent<Tablones>());
-                Destroy(collider);
-
-                if (contador == 8)
+                if (collider.transform.parent.parent.name.Equals("PosicionesCorrectasVentana") && gameObject.transform.parent.parent.name.Equals("SpritesVentana"))
                 {
-                    Debug.Log("Minijuego completado");
+                    UpdateWire(collider.transform.position);    // Actualiza la posición al collider adecuado
+
+                    contador += 1;
+
+                    Destroy(gameObject.GetComponent<Tablones>());
+                    Destroy(collider);
+
+                    if (contador == 8)
+                    {
+                        Debug.Log("Minijuego completado");
+                    }
+
+
+                    return;                                     // Para que el UpdateWire de abajo no se ejecute
                 }
 
-
-                return;                                     // Para que el UpdateWire de abajo no se ejecute
-            }
-
-            else if (collider.transform.parent.parent.name.Equals("PosicionesCorrectasPuerta") && gameObject.transform.parent.parent.name.Equals("SpritesPuerta"))
-            {
-                UpdateWire(collider.transform.position);    // Actualiza la posición al collider adecuado
-
-                contador += 1;
-
-                Destroy(gameObject.GetComponent<Tablones>());
-                Destroy(collider);
-
-                if (contador == 8)
+                else if (collider.transform.parent.parent.name.Equals("PosicionesCorrectasPuerta") && gameObject.transform.parent.parent.name.Equals("SpritesPuerta"))
                 {
-                    Debug.Log("Minijuego completado");
+                    UpdateWire(collider.transform.position);    // Actualiza la posición al collider adecuado
+
+                    contador += 1;
+
+                    Destroy(gameObject.GetComponent<Tablones>());
+                    Destroy(collider);
+
+                    if (contador == 8)
+                    {
+                        Debug.Log("Minijuego completado");
+                    }
+
+
+                    return;                                     // Para que el UpdateWire de abajo no se ejecute
                 }
 
-
-                return;                                     // Para que el UpdateWire de abajo no se ejecute
             }
 
+            UpdateWire(MousePos);
         }
-
-        UpdateWire(MousePos);
+        
 
     }
 
