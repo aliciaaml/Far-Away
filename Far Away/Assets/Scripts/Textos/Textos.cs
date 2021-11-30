941cc56fa8ecd69715781947f933ccb5d6e8ceae
Swappppy <francft1@gmail.com>
2021-11-30 03:52:04 +0100

    initial scripts

---
+++ Far Away/Assets/Scripts/Textos/Textos.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Textos
+{
+    [TextArea(1, 6)]
+    public string[] arrayTextos;
+}
