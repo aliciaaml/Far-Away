e3c5adddf622ca924db743e40ab9357462c163a3
Swappppy <francft1@gmail.com>
2021-12-15 22:31:14 +0100

    Minigame enter on inventory click

---
+++ Far Away/Assets/Scripts/Inventario y objetos/ObjectSlotInteraction.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ObjectSlotInteraction : MonoBehaviour
+{ 
+    bool jugado1 = false;
+    bool jugado2 = true;
+
+    public void ClickTrigger()
+    {
+        if (gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().sprite != null)
+        {
+            if (gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().sprite.name == "Movil" && !jugado1)
+            {
+                gameObject.transform.parent.parent.parent.parent.GetComponent<Animator>().SetBool("Mouse", false);
+                SceneManager.LoadScene("MinijuegoMovil");
+                jugado1 = true;
+            }
+
+            // Esto para la segunda vez que se juega, accede al objeto con este script y cambia jugado2 a false para jugar, como una specie de trigger
+            // Si te es util para como sea que estes haciendo los dialogos puedes meter aqui un trigger para ellos tambien
+            if (gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().sprite.name == "Movil" && jugado1 && !jugado2)
+            {
+                gameObject.transform.parent.parent.parent.parent.GetComponent<Animator>().SetBool("Mouse", false);
+                SceneManager.LoadScene("MinijuegoMovil");
+                jugado2 = true;
+
+
+            }
+        }
+
+        else { return; }
+       
+    }
+}
